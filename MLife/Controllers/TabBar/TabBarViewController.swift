991e0311219a2d6a3e0ccccb2dbaccfb5a07ae91
toannguyen2603 <huutoan2603@gmail.com>
2022-07-27 20:29:45 +0700

    Task_1 Create folder structure for the app

---
+++ MLife/Controllers/TabBar/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  MLife
+//
+//  Created by Nguyễn Hữu Toàn on 27/07/2022.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
---
+++ MLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLife
+//
+//  Created by Nguyễn Hữu Toàn on 27/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
